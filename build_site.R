c0bd797cd5c6563e37c9539e8c2e0386c2922765
GitHub Action - Published website <action@github.com>
2024-06-03 09:16:29 +0000

    Publish website

---
+++ build_site.R
@@ -0,0 +1,10 @@
+source(file.path("R", "functions.R"))
+
+# First build the footer to capture today's date
+create_footer()
+
+# Then render the site
+rmarkdown::render_site(encoding = 'UTF-8')
+
+# Then copy in the redirects file
+fs::file_copy("_redirects", "_site/_redirects")
